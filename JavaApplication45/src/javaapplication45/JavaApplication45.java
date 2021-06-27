db8d2820121d898c2c1cc1f0c6af985428f5f925
hakcinar <44376537+hakcinar@users.noreply.github.com>
2021-06-27 10:54:48 +0300

    Add files via upload

---
+++ JavaApplication45/src/javaapplication45/JavaApplication45.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication45;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Akcinar Ailesi
+ */
+public class JavaApplication45 {
+
+    /**
+     * @param args the command line arguments
+     */
+  public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("N Adedini Girin: ");
+        int n = scan.nextInt();
+        int maks = 0;
+        int min = 0;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + ". sayiyi giriniz: ");
+            int sayi = scan.nextInt();
+
+            if (i == 1) {
+                maks = sayi;
+                min = sayi;
+            }
+
+            if (sayi > maks) {
+                maks = sayi;
+            }
+
+            if (sayi < min) {
+                min = sayi;
+            }
+        }
+
+        System.out.println("En Büyük Sayı: " + maks);
+        System.out.println("En Küçük Sayı: " + min);
+    }
+    
+}
