bbff1312fee425a63eed414c5def63e84efb660e
hakcinar <44376537+hakcinar@users.noreply.github.com>
2021-06-27 11:50:19 +0300

    Add files via upload

---
+++ JavaApplication52/src/javaapplication52/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication52;
+
+
+/**
+ *
+ * @author Akcinar Ailesi
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        MineSweeper oyun = new MineSweeper();
+        oyun.basla();
+    }
+}
